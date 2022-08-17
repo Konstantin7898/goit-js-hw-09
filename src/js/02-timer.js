f3f6da07d4c7fe85bcdde9cf276796ebd850a4b9
Konstantin7898 <lexus7898@gmail.com>
2022-08-18 01:11:33 +0300

    fix timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -10,73 +10,92 @@ const refs = {
   secondsEl: document.querySelector('[data-seconds]'),
 };
 
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0].getTime();
+    const currentDate = Date.now();
+    const isInvalidDate = currentDate > selectedDate;
+
+    refs.button.disabled = isInvalidDate;
+
+    if (isInvalidDate) {
+      alert('Please choose a date in the future');
+    }
+  },
+};
+
+flatpickr(refs.input, options);
+
 const countDownDate = new Date().getTime();
 
 const timeCount = setInterval(() => {
   let now = new Date().getTime();
   let distance = countDownDate - now;
+});
 
-  function convertMs(ms) {
+const timer = {
+  isActive: false,
+  intervalId: null,
+  endTime: 0,
+
+  start(endTime) {
+    const startTime = Date.now();
+    this.endTime = Date.parse(endTime);
+    const deltaTime = this.endTime - startTime;
+    if (deltaTime <= 0) return;
+
+    this.intervalId = setInterval(this.countDown.bind(this), 1000);
+  },
+
+  countDown() {
+    const startTime = Date.now();
+    const deltaTime = this.endTime - startTime;
+    if (deltaTime <= 0) this.stop();
+
+    const timeComponents = this.convertMs(deltaTime);
+    updateClockface(timeComponents);
+  },
+
+  stop() {
+    clearInterval(this.intervalId);
+  },
+
+  convertMs(ms) {
+    // Number of milliseconds per unit of time
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
 
     // Remaining days
-    const days = distance(Math.floor(ms / day));
+    const days = Math.floor(ms / day);
     // Remaining hours
-    const hours = distance(Math.floor(ms % day) / hour);
+    const hours = Math.floor((ms % day) / hour);
     // Remaining minutes
-    const minutes = distance(Math.floor((ms % day) % hour) / minute);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
     // Remaining seconds
-    const seconds = distance(Math.floor(((ms % day) % hour) % minute) / second);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
     return { days, hours, minutes, seconds };
-  }
-});
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-let intervalId = null;
-const timer = {
-  isActive: false,
-
-  start() {
-    const startTime = userSelectedDate;
-
-    intervalId = setInterval(() => {
-      const currentTime = Date.now();
-
-      let diffOfTime = startTime.getTime() - currentTime;
-      if (diffOfTime <= 0) {
-        timer.stop();
-        return;
-      }
-      const timeComponents = convertMs(deltaTime);
-      updateClockface(timeComponents);
-    }, 1000);
-  },
-
-  stop() {
-    clearInterval(intervalId);
   },
 };
 
-refs.input.addEventListener('click', () => {
-  timer.start();
+refs.button.addEventListener('click', () => {
+  // refs.button.disabled = true;
+  timer.start(refs.input.value);
 });
-
-startBtn.disabled = true;
 
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
 
 function updateClockface({ days, hours, minutes, seconds }) {
-  refs.days.textContent = addLeadingZero(days);
-  refs.hours.textContent = addLeadingZero(hours);
-  refs.minutes.textContent = addLeadingZero(minutes);
-  refs.seconds.textContent = addLeadingZero(seconds);
+  refs.value.textContent = addLeadingZero(days);
+  refs.hourseEl.textContent = addLeadingZero(hours);
+  refs.minutesEl.textContent = addLeadingZero(minutes);
+  refs.secondsEl.textContent = addLeadingZero(seconds);
 }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,12 @@
+const refs = {
+  form: document.querySelector('.form'),
+  inputDelay: document.querySelector('input[name = delay]'),
+  inputStep: document.querySelector('input[name = step]'),
+  inputAmount: document.querySelector('input[name = amount]'),
+};
+
+refs.form.addEventListener('submit', submitForm);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
