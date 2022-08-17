4ec1858eff7c449faf8366270fe2c1990cca2474
Konstantin7898 <lexus7898@gmail.com>
2022-08-18 01:22:20 +0300

    Updates

---
+++ 03-promises.4ede1b8a.js
@@ -0,0 +1,2 @@
+({form:document.querySelector(".form"),inputDelay:document.querySelector("input[name = delay]"),inputStep:document.querySelector("input[name = step]"),inputAmount:document.querySelector("input[name = amount]")}).form.addEventListener("submit",submitForm);
+//# sourceMappingURL=03-promises.4ede1b8a.js.map
---
+++ 03-promises.edd2813f.js
@@ -0,0 +1,2 @@
+[document.querySelector(".form"),document.querySelector("input[name = delay]"),document.querySelector("input[name = step]"),document.querySelector("input[name = amount]")][0].addEventListener("submit",submitForm);
+//# sourceMappingURL=03-promises.edd2813f.js.map
