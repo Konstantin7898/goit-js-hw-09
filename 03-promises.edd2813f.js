3b162b39177e79c0f20f5ae2d7d5db187d9c72bd
Konstantin7898 <98184630+Konstantin7898@users.noreply.github.com>
2022-08-18 20:35:16 +0000

    Deploying to gh-pages from @ Konstantin7898/goit-js-hw-09@56ee9b8c38d20ec6d72cfe695ac54085a30c040f 🚀

--- 03-promises.edd2813f.js
+++
@@ -1,2 +0,0 @@
-[document.querySelector(".form"),document.querySelector("input[name = delay]"),document.querySelector("input[name = step]"),document.querySelector("input[name = amount]")][0].addEventListener("submit",submitForm);
-//# sourceMappingURL=03-promises.edd2813f.js.map
